e796a70d85c3618fb7410571058039a8210ddfb2
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-08-06 16:19:18 +0100

    first pass at image downlaod and untar

--- maas-region/src/backups.py
+++ maas-region/src/backups.py
@@ -6,6 +6,7 @@
 import json
 import logging
 import os
+import shutil
 import subprocess
 import tarfile
 import tempfile
@@ -13,6 +14,7 @@ import threading
 from contextlib import nullcontext
 from datetime import datetime
 from io import BytesIO
+from pathlib import Path
 from typing import Any
 
 from boto3.session import Session
@@ -42,6 +44,7 @@ FAILED_TO_ACCESS_CREATE_BUCKET_ERROR_MESSAGE = (
 S3_BLOCK_MESSAGES = [
     FAILED_TO_ACCESS_CREATE_BUCKET_ERROR_MESSAGE,
 ]
+SNAP_COMMON = Path("/var/snap/maas/common/maas")
 SNAP_PATH_TO_IMAGES = "/var/snap/maas/common/maas/image-storage"
 SNAP_PATH_TO_PRESEEDS = "/var/snap/maas/current/preseeds"
 METADATA_PATH = "backup/latest"
@@ -624,7 +627,7 @@ Juju Version: {self.charm.model.juju_version!s}
             logger.exception(e)
             event.fail(f"Failed to list MAAS backups with error: {e!s}")
 
-    def _on_restore_from_backup_action(self, event):
+    def _on_restore_from_backup_action(self, event: ActionEvent) -> None:
         if not self._pre_restore_checks(event):
             return
 
@@ -651,14 +654,147 @@ Juju Version: {self.charm.model.juju_version!s}
 
         self.charm.unit.status = MaintenanceStatus("restoring backup")
 
-        # Step 1
-        logger.info("Step 1")
-        # ...
-        # ...
-        # Step N
-        logger.info("Step N")
+        self._run_restore(event, s3_parameters, backup_id)
 
+        self.charm.unit.status = ActiveStatus()
         event.set_results({"restore-status": "restore finished"})
+
+    def _run_restore(
+        self, event: ActionEvent, s3_parameters: dict[str, str], backup_id: str
+    ) -> None:
+        # Remove relation between maas-region and postgresql. This will stop the maas-region charm based on https://github.com/canonical/maas-charms/pull/511 .
+        if not self._relation_exists("maas-db"):
+            event.fail(
+                "PostgreSQL relation still exists, please run:\n",
+                "juju remove-relation maas-region postgresql\n",
+                "then retry this action",
+            )
+            return
+
+        # Ensure there are the same number of maas-region units as there are controllers in controllers.txt. i.e. 1 for non ha mode, or 3 for ha.
+        path = f"/backup/{backup_id}"
+        controllers_content = self._read_content_from_s3(f"{path}/controllers.txt", s3_parameters)
+        if not controllers_content:
+            event.fail("Restore failed: Could not fetch controllers list")
+            return
+
+        controllers = controllers_content.strip("\n").split("\n")
+        regions = self.model.get_relation("maas-cluster").units
+        if len(controllers) != len(regions):
+            event.fail(
+                f"Restore failed: the number of MAAS-Region units ({len(regions)}) "
+                f"does not match the expected value from the backup ({len(controllers)})."
+            )
+
+        # For each region id in controllers.txt, add the region id to /var/snap/maas/common/maas/maas_id in a relevant maas-region unit.#
+        for region, controller in zip(regions, sorted(controllers)):
+            if self.model.name == region.name:
+                logger.debug(f"Assigning {controller} to {self.model.name}")
+                self._write_region_id(controller)
+
+        # Remove the existing image-storage i.e. sudo rm -rf /var/snap/maas/common/maas/image-storage
+        image_storage = SNAP_COMMON / "image-storage"
+        shutil.rmtree(image_storage, ignore_errors=True)
+        if image_storage.exists():
+            event.fail("Could not remove existing image-storage")
+            return
+
+        # Unpack image-storage.tar.gz into that directory, i.e. sudo tar -zxvf /image-archive.tar.gz -C /
+        ca_chain = s3_parameters.get("tls-ca-chain", [])
+        with tempfile.NamedTemporaryFile() if ca_chain else nullcontext() as ca_file:
+            if ca_file:
+                ca = "\n".join(ca_chain)
+                ca_file.write(ca.encode())
+                ca_file.flush()
+
+                s3 = self._get_s3_session_resource(s3_parameters, ca_file.name)
+            else:
+                s3 = self._get_s3_session_resource(s3_parameters, None)
+
+            if not self._download_and_unarchive_from_s3(
+                event=event,
+                client=s3,
+                s3_path=f"{path}/image-strorage.tar.gz",
+                s3_parameters=s3_parameters,
+                image_storage=image_storage,
+            ):
+                event.fail(
+                    "Failed to download and extract images from S3 backup. Check the juju debug-log for more detail."
+                )
+                return
+
+        # Reintegrate maas-region and postgresql. This should restart maas.
+        event.log(
+            "Backup complete, please run:\n",
+            "juju add-relation maas-region postgresql\n",
+            "to restart MAAS",
+        )
+
+    def _download_and_unarchive_from_s3(
+        self,
+        event: ActionEvent,
+        client: Any,
+        s3_path: str,
+        s3_parameters: dict[str, str],
+        image_storage: Path,
+    ) -> bool:
+        bucket = s3_parameters["bucket"]
+        path = os.path.join(s3_parameters["path"], s3_path).lstrip("/")
+        try:
+            if (
+                (size := client.head_object(Bucket=bucket, key=path)["ContentLenght"])
+                and (free := shutil.disk_usage(image_storage).free)
+                and (size >= free)
+            ):
+                logger.exception(
+                    f"Not enough free storage to extract image archive, required {size} but has {free}"
+                )
+                event.fail("Not enough space to download image archive from S3.")
+                return
+
+            with tempfile.NamedTemporaryFile(suffix=".tar.gz") as f:
+                event.log("Downloading image archive from S3...")
+                client.download_file(bucket, f.name, path)
+
+                event.log("Extracting images from image archive...")
+                with tarfile.open(fileobj=f, mode="r:gz") as tar:
+                    tar.extractall(path="/")
+
+            # check the storage path exists and contains something
+            if image_storage.exists() and any(image_storage.iterdir()):
+                return True
+            logger.error("Image archive from S3 did not contain any files.")
+            event.fail("Empty image archive downloaded from S3")
+            return False
+
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                logger.error(f"Could not find object in {bucket}:{path}")
+                event.fail("Failed to download image archive from S3.")
+
+            else:
+                logger.exception(f"Could not read object from {bucket}:{path}", exc_info=e)
+                event.fail("Failed to download image archive from S3.")
+
+        except (FileNotFoundError, OSError) as e:
+            logger.exception("Filesystem error while extracting archive", exc_info=e)
+            event.fail("Failed to untar image archive from S3.")
+
+        except tarfile.TarError as e:
+            logger.exception("Corrupted or invalid tar archive", exc_info=e)
+            event.fail("Image archive is not a valid .tar.gz file or is corrupted.")
+
+        except Exception as e:
+            logger.exception(f"Could not read content from {bucket}:{path}", exc_info=e)
+            event.fail("Failed to download image archive from S3.")
+
+        return False
+
+    def _relation_exists(self, relation_name: str) -> bool:
+        return self.model.get_relation(relation_name=relation_name) is not None
+
+    def _write_region_id(self, controller_id: str) -> None:
+        (SNAP_COMMON / "maas_id").write_text(f"{controller_id}\n")
 
     def _pre_restore_checks(self, event: ActionEvent) -> bool:
         """Run some checks before starting the restore.
