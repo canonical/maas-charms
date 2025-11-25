ce3ad0bacd329aa43cff4e56e2db4d100eed4df0
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-11-25 10:32:30 +0000

    allow using ingress configurator

--- maas-region/src/charm.py
+++ maas-region/src/charm.py
@@ -20,6 +20,11 @@ from charms.maas_site_manager_k8s.v0 import enroll
 from charms.operator_libs_linux.v2.snap import SnapError
 from charms.tempo_coordinator_k8s.v0.charm_tracing import trace_charm
 from charms.tempo_coordinator_k8s.v0.tracing import TracingEndpointRequirer, charm_tracing_config
+from charms.traefik_k8s.v2.ingress import (
+    IngressPerAppReadyEvent,
+    IngressPerAppRequirer,
+    IngressPerAppRevokedEvent,
+)
 from ops.model import SecretNotFoundError
 
 from backups import MAASBackups
@@ -30,6 +35,7 @@ logger = logging.getLogger(__name__)
 MAAS_PEER_NAME = "maas-cluster"
 MAAS_API_RELATION = "api"
 MAAS_DB_NAME = "maas-db"
+MAAS_INGRESS_RELATION = "ingress"
 
 MAAS_SNAP_CHANNEL = "3.6/stable"
 
@@ -139,6 +145,15 @@ class MaasRegionCharm(ops.CharmBase):
         self.framework.observe(api_events.relation_changed, self._on_api_endpoint_changed)
         self.framework.observe(api_events.relation_departed, self._on_api_endpoint_changed)
         self.framework.observe(api_events.relation_broken, self._on_api_endpoint_changed)
+
+        # Ingress
+        self.ingress = IngressPerAppRequirer(
+            self,
+            relation_name=MAAS_INGRESS_RELATION,
+            scheme="http",
+        )
+        self.framework.observe(self.ingress.on.ready, self._on_ingress_ready)
+        self.framework.observe(self.ingress.on.revoked, self._on_ingress_revoked)
 
         # COS
         endpoints: list[cos_agent._MetricsEndpointDict] = [
@@ -343,6 +358,14 @@ class MaasRegionCharm(ops.CharmBase):
         )
 
     def _update_ha_proxy(self) -> None:
+        # set up the ingress
+        http = self.config["tls_mode"] in ["disabled", "termination"]
+        self.ingress.provide_ingress_requirements(
+            port=MAAS_HTTP_PORT if http else MAAS_HTTPS_PORT,
+            scheme="http" if http else "https",
+        )
+
+        # XXX: Is this superfluous
         region_port = (
             MAAS_HTTPS_PORT if self.config["tls_mode"] == "passthrough" else MAAS_HTTP_PORT
         )
@@ -489,6 +512,14 @@ class MaasRegionCharm(ops.CharmBase):
         logger.info(event)
         self._update_ha_proxy()
         self._initialize_maas()
+
+    def _on_ingress_ready(self, event: IngressPerAppReadyEvent) -> None:
+        logger.info(event)
+        logger.info(f"Ingress available: {event.url}")
+
+    def _on_ingress_revoked(self, event: IngressPerAppRevokedEvent) -> None:
+        logger.info(event)
+        logger.info("Ingress revoked for unit")
 
     def _on_maas_peer_changed(self, event: ops.RelationEvent) -> None:
         logger.info(event)
