c8d55bac16e15a5c20865e6925a55b027d4893f2
Andrew Lamzed-Short <andrew.lamzedshort@canonical.com>
2025-12-05 15:47:32 +0000

    chore: upgrade maas-region charm to 3.7/stable channel (#565)

--- maas-region/tests/integration/test_charm.py
+++ maas-region/tests/integration/test_charm.py
@@ -42,7 +42,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
 async def test_charm_version_is_set(ops_test: OpsTest):
     status = await ops_test.model.get_status()
     version = status.applications[APP_NAME].workload_version
-    assert version.startswith("3.6.")
+    assert version.startswith("3.7.")
 
 
 @pytest.mark.abort_on_fail
