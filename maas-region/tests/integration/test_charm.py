bdd3a431ca2d1961cf4b864034067b4a6f45b86c
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2026-02-11 16:57:58 +0000

    deploy haproxy and then wait

--- maas-region/tests/integration/test_charm.py
+++ maas-region/tests/integration/test_charm.py
@@ -117,17 +117,10 @@ async def test_haproxy_integration(ops_test: OpsTest, tmp_path):
     """Verify that the charm haproxy integration works as expected."""
     cert, key = generate_cert(tmp_path=tmp_path)
 
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-    )
+    # Deploy the charm and haproxy and wait for active/waiting status
     await ops_test.model.applications[APP_NAME].set_config(
         {"ssl_cert_content": cert, "ssl_key_content": key}
     )
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-    )
-
-    # Deploy the charm and haproxy and wait for active/waiting status
     await ops_test.model.deploy(
         "haproxy",
         application_name="haproxy",
@@ -136,7 +129,7 @@ async def test_haproxy_integration(ops_test: OpsTest, tmp_path):
         trust=True,
     )
     await ops_test.model.wait_for_idle(
-        apps=["haproxy"], status="active", raise_on_blocked=True, timeout=1000
+        apps=["haproxy", APP_NAME], status="active", raise_on_blocked=True, timeout=1000
     )
 
     await ops_test.model.integrate(f"{APP_NAME}:ingress-tcp", "haproxy")
