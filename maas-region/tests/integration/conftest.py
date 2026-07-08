7b783eeba667469ac98f280ab3a566d08e257dbb
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2026-07-08 10:37:25 +0100

    update licence and testing locations

--- maas-region/tests/integration/conftest.py
+++ maas-region/tests/integration/conftest.py
@@ -4,6 +4,21 @@
 from pathlib import Path
 
 import yaml
+from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption(
+        "--charm-file",
+        action="store",
+        help="Path to a pre-built charm file",
+    )
+    parser.addoption(
+        "--series",
+        action="store",
+        help="Ubuntu series to run tests on",
+    )
+
 
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
