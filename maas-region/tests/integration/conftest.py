6f21a861f594c40b8e897377c26b87d1a17505ef
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2026-07-10 17:51:55 +0100

    refactor: Update CI for modern best-practices (#649)
    * replace pull-request.yaml with operator-workflows
    
    * replace release.yaml with operator-workflows
    
    * o replace update-libs.yaml with operator-workflows
    
    * align tox.ini with operator-workflows expectations
    
    * promote to operator-workflows; add dependabot
    
    * add track input to promote workflow
    
    * cleanup: rm unused pyrightconfig, update contributing
    
    * reorder tox
    
    * pin operator-workflows to commit SHA
    
    * annotate pinned commit SHAs with date
    
    * add comments explaining the code
    
    * fix indent
    
    * licence reqs
    
    * update licence and testing locations
    
    * add header to licence.yaml
    
    * now try
    
    * update licence years
    
    * eof fix
    
    * drop dependabot, keep renovate
    
    * licence year and renovate weekly
    
    * add commented-out single-PR config to description
    
    * clean up renovate config
    
    * renovate.json -> renovate.json5 for JSON5 comments
    
    * single weekly PR with catch-all group
    
    * respond to nits

--- maas-region/tests/integration/conftest.py
+++ maas-region/tests/integration/conftest.py
@@ -1,6 +1,24 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 from pathlib import Path
 
 import yaml
+from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption(
+        "--charm-file",
+        action="store",
+        help="Path to a pre-built charm file",
+    )
+    parser.addoption(
+        "--series",
+        action="store",
+        help="Ubuntu series to run tests on",
+    )
+
 
 METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
