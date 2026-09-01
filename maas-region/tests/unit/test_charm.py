d0347445de0315663199e1ab01e2f33c8f754a63
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2026-09-01 11:49:46 +0300

    chore: remove temporary fix for resolute (#678)
    * chore: remove temporary fix for resolute
    
    * fix CI errors

--- maas-region/tests/unit/test_charm.py
+++ maas-region/tests/unit/test_charm.py
@@ -807,9 +807,7 @@ class TestMAASURLs(unittest.TestCase):
                 try:
                     harness.begin()
                     rel_id = harness.add_relation(HAPROXY_NON_TLS, "haproxy")
-                    harness.update_relation_data(
-                        rel_id, "haproxy", {"endpoints": endpoints_value}
-                    )
+                    harness.update_relation_data(rel_id, "haproxy", {"endpoints": endpoints_value})
                     self.assertEqual(
                         harness.charm.maas_cli_url, f"http://10.0.0.10:{MAAS_HTTP_PORT}/MAAS"
                     )
@@ -856,9 +854,7 @@ class TestMAASURLs(unittest.TestCase):
                 try:
                     harness.begin()
                     rel_id = harness.add_relation(HAPROXY_NON_TLS, "haproxy")
-                    harness.update_relation_data(
-                        rel_id, "haproxy", {"endpoints": endpoints_value}
-                    )
+                    harness.update_relation_data(rel_id, "haproxy", {"endpoints": endpoints_value})
                     self.assertEqual(
                         harness.charm.maas_api_url, f"http://10.0.0.10:{MAAS_HTTP_PORT}/MAAS"
                     )
